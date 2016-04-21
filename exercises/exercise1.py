cef222d9d3aa96494e3e2112c7f0e0a65dffdbce
Tomer Schlesinger <tomer.schles@gmail.com>
2016-04-21 10:16:24 +0300

    Updated .gitignore & Exercise #1 started
    First Exercise started
    Updated .gitignore for a pycharm project (as well as for python)

---
+++ exercises/exercise1.py
@@ -0,0 +1,11 @@
+"""
+
+Tomer's Interactive python quickstart guide
+
+        Exercise #1 - Basic Food Ordering System
+        String input, parsing, lists & dictionaries
+
+
+    Each line starts with
+
+"""
